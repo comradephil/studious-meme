905b7b92902705fa5338447767cf6bd8fdde48d7
Phil Bill <pdbill@hotmail.com>
2017-02-09 21:06:04 +0000

    Added basic validation to PHP
    Added some basic validation to the PHP to prevent poor inputs, also to ensure that certain characters are correctly escaped for SQL query.

--- postmessages.php
+++ postmessages.php
@@ -10,13 +10,26 @@ if ($dbconn->connect_error) {
 }
 global $dbconn;
 // collect value of input field
-$user = $_POST['user'];
-$title = $_POST['title'];
-$content = $_POST['post'];
+$user = $dbconn->real_escape_string(test_input($_POST['user']));
+$title = $dbconn->real_escape_string(test_input($_POST['title']));
+$content = $dbconn->real_escape_string(test_input($_POST['post']));
 
-$query = "INSERT INTO `newsposts`(`USER`, `TITLE`, `CONTENT`) VALUES ('$user','$title','$content');";
-mysqli_query($dbconn,$query);
+function test_input($data) {
+	$data = trim($data);
+	$data = stripslashes($data);
+	$data = htmlspecialchars($data);
+	return $data;
+}
+
+
+$query = "INSERT INTO `newsposts`(`USER`, `TITLE`, `CONTENT`) VALUES ('" . $user . "','" . $title . "','" . $content . "');";
+$success = mysqli_query($dbconn,$query);
 mysqli_close($dbconn);
-echo "Post Submitted!";
+if($success)
+	echo "Post Submitted!";
+else {
+	echo "Post failed";
+}
 
 ?>
+
